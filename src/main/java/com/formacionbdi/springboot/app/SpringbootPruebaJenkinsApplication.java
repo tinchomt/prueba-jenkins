939d1d6351d1bf2018d574dd2603fc081bfb0dd5
tinchomt <tincho.mt@gmail.com>
2020-03-10 10:07:15 -0300

    Cambios

---
+++ src/main/java/com/formacionbdi/springboot/app/SpringbootPruebaJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPruebaJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPruebaJenkinsApplication.class, args);
+	}
+
+}
